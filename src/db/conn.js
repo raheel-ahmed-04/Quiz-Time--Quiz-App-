2cd09ee9776c827aa22b0a773f2dec5ed7ea7847
Raheel Ahmed <raheelqazi29@gmail.com>
2024-07-27 00:45:11 +0500

    #

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // "mongodb+srv://Taha9753:Unchartered3.@tahacluster.wxaybwk.mongodb.net/quiztime",
-      "mongodb+srv://raheelgenius747:Raheel123@raheelcluster.66esims.mongodb.net/QuizTime",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      // "mongodb+srv://Taha9753:Unchartered3.@tahacluster.wxaybwk.mongodb.net/quiztime"
+      "mongodb+srv://raheelgenius747:Raheel123@raheelcluster.66esims.mongodb.net/QuizTime"
+      // ,{
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // }
     );
     console.log(`The DB is connected with ${mongoose.connection.host}`);
   } catch (error) {
